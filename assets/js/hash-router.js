3194f43010c58bf8470c5b2c87b9562d1d2f9132
Pedro Medina Robles <pedromedina@axosyscom.com>
2024-06-25 20:57:14 -0600

    [adv]: concepto general de la pagina

---
+++ assets/js/hash-router.js
@@ -0,0 +1,44 @@
+const pageTitle = "FILA";
+const routes = {
+	404: {
+		template: "/templates/404.html",
+		title: "404 | " + pageTitle,
+		description: "Page not found",
+	},
+	"/": {
+		template: "/templates/index.html",
+		title: "Home | " + pageTitle,
+		description: "This is the home page",
+	},
+	nosotros: {
+		template: "/templates/nosotros.html",
+		title: "Nosotros | " + pageTitle,
+		description: "This is the about page",
+	},
+	nosotros: {
+		template: "/templates/store.html",
+		title: "Store | " + pageTitle,
+		description: "This is the about page",
+	},
+	contacto: {
+		template: "/templates/contacto.html",
+		title: "Contacto | " + pageTitle,
+		description: "This is the contact page",
+	},
+};
+
+const locationHandler = async () => {
+	var location = window.location.hash.replace("#", "");
+	if (location.length == 0) {
+		location = "/";
+	}
+	const route = routes[location] || routes["404"];
+	const html = await fetch(route.template).then((response) => response.text());
+	document.getElementById("data-page").innerHTML = html;
+	document.title = route.title;
+	document
+		.querySelector('meta[name="description"]')
+		.setAttribute("data-page", route.description);
+};
+window.addEventListener("hashchange", locationHandler);
+locationHandler();
